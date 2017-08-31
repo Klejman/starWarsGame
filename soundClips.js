42f8ec2de17145b10716fad8bd227e4127e86a4f
Katie Lejman <misskatiemary@gmail.com>
2017-08-31 11:41:12 -0400

    collected assets sound and images to use in the game.

---
+++ soundClips.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+$("#my_audio").get(0).play();//.prop('volume', 0.03);
+
+// if(userScore == randomNum) {
+// $("#my_audio")("src", "assets/music/winToot.mp3");
+
+// }else if(userScore > randomNum) {
+// $("#my_audio")("src", "assets/music/ohNo.mp3");
+ 
+// }
+});	
+
+ // if(userScore == randomNum) {
+//  winTootMP3.play();	
+//  $("#my_audio")("src", "assets/sound.mp3");
+// } else if(userScore > randomNum){
+// ohNoMP3.play();
+// }
+// }
+// });
